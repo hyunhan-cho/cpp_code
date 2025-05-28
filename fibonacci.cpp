4bc333024afdc230a9fe67a10abd2fa7404cbeaa
hyunhan-cho <hyeonhanjo1@gmail.com>
2025-05-28 12:34:38 +0900

    Initial commit

---
+++ fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int fibonacci(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    return fibonacci(n-1)+fibonacci(n-2);
+}
+
+int main(){
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Fibonacci of " << n << " is " << fibonacci(n) << endl;
+    return 0;
+}   
